86e46e16bd4b6aa1c4d5dc414d9705b71d0a2a37
Axda <axda.web@gmail.com>
2022-02-16 22:37:06 +0100

    Issue #2 Implémenter entrées du form + Issue #3 Ajout validation / message d'erreur OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,9 +14,15 @@ const formData = document.querySelectorAll(".formData");
 
 const closeBtn = document.querySelector('.form-modal-close')
 const form = document.getElementById('form')
+const inputs = document.querySelectorAll('input:not([type="submit"], [id="checkbox2"])')
+const locationErrMessage = document.getElementById('location-error-message')
+const radioCheckboxes = document.querySelectorAll('.checkbox-label .checkbox-icon')
+const allErrorMessage = Array.from(document.querySelectorAll('.error-message'))
+const confirmationModal = document.querySelector('.submit-confirmation-container')
+const closeConfirmationModal = document.querySelector('.submit-confirmation__btn')
+const closeBtnPostSubmit = document.querySelector('.submit-confirmation-close')
+const locationArr = []
 
-  
-console.log(closeBtn)
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -26,6 +32,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
 // Close modal
 
 const closeModal = event => {
@@ -33,3 +40,60 @@ const closeModal = event => {
 }
 
 closeBtn.addEventListener('click', closeModal)
+
+
+
+// Check inputs validity
+
+const setErrorStyle = input => {
+  input.closest('.formData').classList.remove('success')
+  input.closest('.formData').classList.add('error')
+}
+
+const setSuccessStyle = input => {
+  input.closest('.formData').classList.remove('error')
+  input.closest('.formData').classList.add('success')
+}
+
+
+const checkInputs = () => {
+  
+  for(let input of inputs){
+  
+    // Check the validity of location input (part 1)
+    if(input.type === 'radio'){
+      locationArr.push(input.checked)
+    }
+
+    //Check the validity of all the inputs
+    if(!input.checkValidity()){
+      setErrorStyle(input)
+    } else {
+      setSuccessStyle(input)
+    }
+  }
+
+  // Check the validity of location input (part 2)
+  if(!locationArr.some(item => item === true)){
+    locationErrMessage.style.display = 'block'
+    for(let radioCheckbox of radioCheckboxes){
+      radioCheckbox.style.border = "2px solid red"
+    }
+  } else {
+    locationErrMessage.style.display = 'none'
+    for(let radioCheckbox of radioCheckboxes){
+      radioCheckbox.style.border = "2px solid #279e7a"
+    }
+  }
+}
+
+
+
+// Manage form submission
+
+const formSubmit = e => {
+  e.preventDefault()
+  checkInputs()
+}
+
+form.addEventListener('submit', formSubmit)
